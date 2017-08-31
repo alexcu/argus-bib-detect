3f3c9b724a41611f6c67502966e8261be946d61b
Luis Torres <luisfciencias@gmail.com>
2017-08-31 13:33:46 +1000

    ENHANCE: Improves preprocessed image for tesseract with gray scale operations

--- preprocess.py
+++ preprocess.py
@@ -46,6 +46,78 @@ def preprocess(img):
     # Return the processed image image
     return threshold
 
+def multi_preprocess(img):
+    """
+    Produces candidate images for input to tesseract
+    input:
+        img (array)
+    output: dictionary of processed images with the following keys
+        'color' : original image
+        'gray'  : gray scale image
+        'gray_inv': inversion of the gray scale image (white <--> black)
+        'blur_gauss': gaussian blurring (convolution),
+        'blur_gauss_inv': inversion of the gaussian blurring (white <--> black),
+        'blend': sketch transformation
+        'thresh': threshold version
+        'eq_hist': equalization histogram
+    Suggestion from exploratory phase: consider gray and gray inv as the top two options.
+    If the quality of the bib is high, all the previous choices generate positive results
+    """
+    # gray scale conversion
+    img_gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    # gray scale inverted
+    img_gray_inv = cv2.bitwise_not(img_gray)
+    
+    # equalization histogram
+    eq_hist = cv2.equalizeHist(img_gray)
+    
+    #blurring to increase edges and color contrast:'gaussian convolution' with a kernel
+    # in this case the filter (5,5) works fine and the sigma parameters extracted
+    # from the image. That is why they are set to zero
+    img_blur = cv2.GaussianBlur(img_gray,ksize=(5,5),sigmaX=0,sigmaY=0)
+    
+    # inversion
+    img_blur_inv = cv2.bitwise_not(img_blur)
+    
+    # statistical flag for white versus black ID: mean works better than median
+    # this will only make sense for the cropped bib digits region
+    flag_background = np.mean(img_gray)
+    # RGB scale values:
+    # black=(0,0,0)   white=(255,255,255)
+    # initial idea: 128-ish is half the size of the RGB scale so:
+    if flag_background > 128:
+        print('White Number')
+        ret1 , th1 = cv2.threshold(img_gray,220,255,cv2.THRESH_BINARY_INV)
+    else:
+        print('Black Number')
+        ret1 , th1 = cv2.threshold(img_gray,128,255,cv2.THRESH_BINARY)
+        
+    # blending the image with its convolution: sketch transformation
+    # this was useful when the bib region was much bigger
+    # probably not so useful when the bib region is much smaller
+    img_blend = cv2.divide(img_gray_inv,255-img_blur,scale=256)
+    
+    # output: ideally a very clean threshold version is the input for tesseract
+    # although if the bib is clean the gray scale images can be fed into
+    # tesseract also with positive results
+    dic_images = {'color': img ,
+                  'gray': img_gray,
+                  'gray_inv': img_gray_inv,
+                  'blur_gauss': img_blur,
+                  'blur_gauss_inv':img_blur_inv,
+                  'blend': img_blend,
+                  'thresh': th1,
+                  'eq_hist': eq_hist}
+    
+    return dict_images
+
+# usage:
+#-- generating dictionary
+#dic_images = multi_process(img)
+#-- calling the top two candidate images from the dictionary
+#img_tesseract_input_gray = dic_images['gray']
+#img_tesseract_input_gray_inv = dic_images['gray_inv']
+
 def main():
     assert len(sys.argv) - 1 >= 2, "Must provide two arguments (in_dir, out_dir)"
 
